c0eec0b9f3da67de32697a0c5f5fd0fd3f520e63
AbenezerM <abenezermulugeta41@gmail.com>
2023-09-23 01:31:26 -0400

    Define a security filter in payment service.

---
+++ paymentservice/src/main/java/com/abenezersefinew/paymentservice/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.abenezersefinew.paymentservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    /**
+     * Protects endpoints when making internal calls within the microservice
+     * (for instance, calls coming from order service).
+     * */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers("/payments/**")
+                .hasAuthority("SCOPE_internal")
+                .anyRequest()
+                .authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
+    }
+}
